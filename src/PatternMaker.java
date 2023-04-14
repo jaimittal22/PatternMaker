4c1c8baa13e1db77d3a16b0118734b94fe241d36
jaimittal <jai_mittal24@milton.edu>
2023-04-14 13:06:27 -0400

    inital commit

--- src/PatternMaker.java
+++ src/PatternMaker.java
@@ -10,6 +10,9 @@ public class PatternMaker {
         pattern2();
         pattern3();
         pattern5();
+        pattern6();
+        pattern6();
+        pattern7();
     }
 
     public void pattern1() {
@@ -40,6 +43,27 @@ public class PatternMaker {
             for (int c = 1; c < 5; c++) {
                 System.out.print(c + "\t");
             }
+        }
+    }
+
+    public void pattern6() {
+        for (int z = 0; z < 6; z++) {
+            for (int c = 1; c < z; c++) {
+                System.out.print(" ");
+            }
+            System.out.println(z);
+
+        }
+
+
+    }
+
+    public void pattern7() {
+        for (int p = 1; p < 6; p++) {
+            for (int f = 0; f < 6 - p; f++) {
+                System.out.print(p);
+            }
+            System.out.println();
         }
     }
 }
